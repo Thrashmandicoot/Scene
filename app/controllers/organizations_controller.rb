eb16f4aacffe22aabe89e74c31300f6851ff4466
Alex DeLaPena & Ashton Alexander <pair+ajdlp.thrashmandicoot@devbootcamp.com>
2015-08-28 12:11:55 -0700

    Define new, create, edit, update, and org_params to org ctrl

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -6,17 +6,37 @@ class OrganizationsController < ApplicationController
   end
 
   def new
+    @organization = Organization.new
   end
 
   def create
+    @organization = Organization.new(organization_params)
+    if @organization.save
+      redirect_to organizations_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @organization = Organization.find_by(id: params[:id])
   end
 
   def update
+    @organization = Organization.find_by(id: params[:id])
+    if @organization.update(organization_params)
+      redirect_to organizations_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def organization_params
+    params.require(:organizations).permit(:name, :avatar, :bio, :address, :email, :password, :twitter, :facebook, :website)
   end
 end
