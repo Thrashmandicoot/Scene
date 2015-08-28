26d89fc260e920466d41656026b61b151df710c3
Andrew Pham <email": null>
2015-08-28 14:56:33 -0700

    will show all pieces from an artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-artist1 = Artist.create(name: "Banksy", email: "banksy@street.org", password: "admin", avatar: "this is the avatar", bio: "I am a street artist", address: "Address Unknown", facebook: "banksy@facebook", twitter: "banksy@twitter", website: "banksyart.com")
-artist1.pieces.create(img: "http://news.bbcimg.co.uk/media/images/70222000/jpg/_70222444_banksy.jpg", title: "Piece Title", description: "This is the description")
+artist1 = Artist.create(name: "Banksy", email: "banksy@street.org", password: "admin", avatar: "Banksy's Avatar", bio: "I am a street artist", address: "Address Unknown", facebook: "banksy@facebook", twitter: "banksy@twitter", website: "banksyart.com")
+artist1.pieces.create(img: "http://news.bbcimg.co.uk/media/images/70222000/jpg/_70222444_banksy.jpg", title: "No Graffiti", description: "This is the description")
+
+artist1.pieces.create(img: "http://www.opticalspy.com/uploads/1/4/4/9/144966/_2912056_orig.jpg", title: "Maid", description: "This is the description")
