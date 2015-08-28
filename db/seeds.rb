93592978b93c53f8b2a6d865670a7ed53445c34c
ormaechea <castilloandres17@gmail.com>
2015-08-28 16:54:29 -0700

    Add scene#show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,1 @@
 
-Organization.create( name: "dbc", bio:"hello",avatar:"hello", address:"hello",email:"hello", password_digest:"hello", twitter:"hello", facebook:"hello", website:"hello" )
-
-
-10.times do
-  OrgScene.create(img: Faker::Avatar.image, title: Faker::Name.title, guidelines: Faker::Lorem.sentence, organization_id:1)
-end
-
-
-
