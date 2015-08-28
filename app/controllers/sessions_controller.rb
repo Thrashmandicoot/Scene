eed3429d1af21139e429bb48c9275f65c2d5ab52
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-28 15:34:50 -0700

    Add login and logout features including routes, authentication, and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,31 @@
 class SessionsController < ApplicationController
-  def new
+
+  def create
+  	if Artist.find_by(email: params[:session][:email]).present?
+  		user = Artist.find_by(email: params[:session][:email])
+  		if user && user.authenticate(params[:session][:password])
+  			session[:user_id] = user.id
+  			redirect_to artist_path(user)
+  		else
+  			redirect_to '/login'
+  		end
+  	elsif Organization.find_by(email: params[:session][:email]).present?
+  		user = Organization.find_by(email: params[:session][:email])
+  		if user && user.authenticate(params[:session][:password])
+  			session[:user_id] = user.id
+  			redirect_to organization_path(user)
+  		else
+  			redirect_to '/login'
+  		end
+  	else
+ 		flash.now[:danger] = 'Invalid email/password combination'
+ 		redirect_to '/login'
+  	end
   end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to '/login'
+  end
+
 end
