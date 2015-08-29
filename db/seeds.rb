58185a6a4e72f7ac5e70d902a8935a9d26e3b2f8
ormaechea <castilloandres17@gmail.com>
2015-08-28 21:59:02 -0700

    Replace Orgscenes with Spaces succesfully in all the files of the project

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 Organization.create(name:"DBC", bio:"DBC", avatar:"DBC", address:"DBC",email:"DBC",password_digest:"DBC", twitter:"DBC", facebook:"DBC", website:"DBC")
 
 10.times do
- Orgscene.create(img: Faker::Avatar.image, title:Faker::Name.title, guidelines: Faker::Lorem.sentence, organization: 1)
+ Space.create(img: Faker::Avatar.image, title:Faker::Name.title, guidelines: Faker::Lorem.sentence, organization_id: 1)
 
 end
