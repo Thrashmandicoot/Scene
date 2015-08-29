6595c23e72dce6d24cdc51715953212f0e1f8c68
Alex DeLaPena & Ashton Alexander <pair+ajdlp.thrashmandicoot@devbootcamp.com>
2015-08-28 22:14:33 -0700

    Define show, new, create, edit, update, and destroy methods in ArtistsController

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,20 +3,44 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
-  end
-
-  def edit
+    @artist = Artist.new
   end
 
   def create
+    @artist = Artist.new(artist_params)
+    if @arist.save
+      redirect_to artist_path(@artist)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @artist = Artist.find_by(id: params[:id])
   end
 
   def update
+    @artist = Artist.find_by(id: params[:id])
+    if @artist.update(artist_params)
+      redirect_to artist_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @artist = Artist.find_by(id: params[:id])
+    @artist.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def organization_params
+    params.require(:artist).permit(:name, :email, :password, :address, :facebook, :twitter, :website, :bio, :avatar)
   end
 end
