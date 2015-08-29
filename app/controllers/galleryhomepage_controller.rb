41a720d7aebb8ee6e20119be74811bec6053035b
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-29 11:05:37 -0700

    Add gallery homepage css and view

--- app/controllers/galleryhomepage_controller.rb
+++ app/controllers/galleryhomepage_controller.rb
@@ -1,4 +1,7 @@
 class GalleryhomepageController < ApplicationController
   def index
+  	@random_pieces = Piece.all.sample(5)
+  	@random_scenes = Orgscene.all.sample(5)
+  	@gallery_images = Piece.all
   end
 end
