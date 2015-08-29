6c1101840c5ac5e5cd8b251be1ef8f5b044fd40f
ormaechea <castilloandres17@gmail.com>
2015-08-28 21:42:01 -0700

    Fix orgscene issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
 
+Organization.create(name:"DBC", bio:"DBC", avatar:"DBC", address:"DBC",email:"DBC",password_digest:"DBC", twitter:"DBC", facebook:"DBC", website:"DBC")
+
+10.times do
+ Orgscene.create(img: Faker::Avatar.image, title:Faker::Name.title, guidelines: Faker::Lorem.sentence, organization: 1)
+
+end
