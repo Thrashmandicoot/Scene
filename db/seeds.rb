426fc9e1990c72a9233461c594df503b00a201a6
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-29 11:33:35 -0700

    Edit module name for Scenes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 artist1 = Artist.create(name: "Banksy", email: "banksy@street.org", password: "admin", avatar: "this is the avatar", bio: "I am a street artist", address: "Address Unknown", facebook: "banksy@facebook", twitter: "banksy@twitter", website: "banksyart.com")
-artist1.pieces.create(img: "This is the image", title: "Piece Title", description: "This is the description")
+# artist1.pieces.create(img: "This is the image", title: "Piece Title", description: "This is the description")
 
 organization1 = Organization.create(
 	name: "Costco",
