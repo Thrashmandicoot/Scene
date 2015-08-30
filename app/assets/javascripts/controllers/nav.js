5722ed008307f9d3e04d02a5064db945e7cf38de
Alex DeLaPena <ajdlp12@gmail.com>
2015-08-29 21:25:39 -0700

    Refactor NAV bar to be railsified

--- app/assets/javascripts/controllers/nav.js
+++
@@ -1,6 +0,0 @@
-app.controller('HeaderCtrl', function HeaderController($scope, $location)
-{
-    $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
-    };
-});
