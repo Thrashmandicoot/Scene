f232d2e6d62c905583f18eb53131c5d41751797a
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-30 23:24:20 -0700

    Carousel refactor

--- app/assets/javascripts/controllers/carousel.js
+++ app/assets/javascripts/controllers/carousel.js
@@ -1,20 +1,12 @@
-gallery.controller('CarouselDemoCtrl', function ($scope, $http) {
-  $scope.myInterval = 2500;
+angular.module('galleryApp', ['ui.bootstrap', 'ngAnimate']).controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   var slides = $scope.slides = [];
 
-  var json_stuff = $http.get('/api/pieces').success(function(data){
-    $scope.addSlide = function(fimages) {
-      var newWidth = 600 + slides.length + 1;
-      slides.push({
-        image: fimages.img,
-        text: fimages.title
-      });
-    };
-    for (var i=0; i<10; i++) {
-      $scope.addSlide(data[i]);
-    }
-    }
-  );
-
+  Gallery.images.forEach(function(image) {
+    slides.push({
+      image: image.img,
+      text: image.title
+    });
+  });
 });
