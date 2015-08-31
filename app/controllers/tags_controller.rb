75bb3732d3f78874bcf331411c4abe106b5b143d
Andrew Pham <email": null>
2015-08-31 13:52:23 -0700

    allow pieces to be filtered by tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,15 +1,13 @@
 class TagsController < ApplicationController
-
+  include TagsHelper
   def show
-
     pieces = Piece.all
     @pieces = []
     pieces.each do |piece|
       (@pieces << piece) if piece.tag_list.include?(params[:id])
     end
     @tag = params[:id]
+    pull_tags(pieces)
     @pieces
   end
-
-
 end
