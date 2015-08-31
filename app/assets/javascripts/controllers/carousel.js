411b94256515f121eebbc1fbea6ebdd21d18727e
Ashton Alexander <ashton.alexander@gmail.com>
2015-08-30 22:48:04 -0700

    Remove console.logs

--- app/assets/javascripts/controllers/carousel.js
+++ app/assets/javascripts/controllers/carousel.js
@@ -1,12 +1,10 @@
 gallery.controller('CarouselDemoCtrl', function ($scope, $http) {
-  console.log("WE MADE IT.  ")
   $scope.myInterval = 2500;
   $scope.noWrapSlides = false;
   var slides = $scope.slides = [];
 
   var json_stuff = $http.get('/api/pieces').success(function(data){
     $scope.addSlide = function(fimages) {
-      console.log(data);
       var newWidth = 600 + slides.length + 1;
       slides.push({
         image: fimages.img,
