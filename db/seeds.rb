f57147380ad4da71654f1cf6aed06d2a51063f96
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-31 02:25:21 -0700

    Update Seedfile with scraped data for Artists, Organizations, Pieces, and Scenes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#=================ADMIN LOGIN
+
 Organization.create(
 	name: "Admin",
 	bio: Faker::Company.catch_phrase,
@@ -8,18 +10,11 @@ Organization.create(
 	website: Faker::Internet.domain_name
 	)
 
-Artist.create(
-		name: "Artist",
-		bio: Faker::Lorem.paragraph,
-		avatar: Faker::Avatar.image,
-		address: Faker::Address.city,
-		email: "artist@artist.org",
-		password: "123456"
-		)
+#=================ORGANIZATIONS + SCENES
 
-10.times do
+30.times do
 	Organization.create(
-		name: Faker::Company.name,
+		name: Faker::Company.name + ['Immersion', 'Art', 'Imaginarium', 'Nose', 'Officetorium'].sample,
 		bio: Faker::Company.catch_phrase,
 		avatar: Faker::Company.logo,
 		address: Faker::Address.city,
@@ -29,30 +24,7 @@ Artist.create(
 	)
 end
 
-10.times do
-	Artist.create(
-		name: Faker::Name.name,
-		bio: Faker::Lorem.paragraph,
-		avatar: Faker::Avatar.image,
-		address: Faker::Address.city,
-		email: Faker::Internet.safe_email,
-		password: Faker::Internet.password(6)
-	)
-end
-
-
-tags = ["Street", "Fine Art", "Graffiti", "Abstract", "Scenic", "Minimal"].sample(2)
-15.times do
-	piece = Piece.create(
-	artist_id: (1..10).to_a.sample,
-	img: Faker::Avatar.image,
-	title: Faker::Lorem.sentence,
-	description: Faker::Lorem.sentence(3),
-	tag_list: tags
-	)
-end
-
-15.times do
+30.times do
 	Space.create(
 	img: Faker::Avatar.image,
 	title: Faker::Lorem.sentence,
@@ -61,4 +33,41 @@ end
 	)
 end
 
+#=================ARTISTS + PIECES + TAGS
+
+random_descriptions = ['Canvas', 'Card stock', 'Fabric' , 'Glass', 'Human body', 'Metal', 'Paper', 'Plaster', 'Scratchboard', 'Vellum', 'Walls', 'Wood', 'Airbrush', 'Acrylic paint', 'Charcoal', 'Colored pencil', 'Conté', 'Crayon', 'Gouache', 'Graphite', 'Human finger', 'Marker', 'Oil paint', 'Pastel', 'Pen and ink', 'Pencil', 'Sand', 'Watercolor', 'Tempera']
+
+random_tags = ['Graffiti', 'Magick Realizm', 'Glitch', 'Figurative', 'Folk', 'Religious', 'Avant Garde', 'Participatory', 'Street', 'Southern', 'Micography', 'Marine Art', 'Fine Art', 'Antiquities', 'Animal Style', 'Cybersexual', 'Eclecticism in art', 'New Media', 'Victimless', 'Painted', 'Nouvelle Vagrance', 'New Chimney', 'Shoe', 'General Surveillance', 'Sound Art', 'Pleorama', 'Dada', 'Massurealism', 'Decorative', 'Elvis', 'Mall Goth', 'Environmental']
+
+random_addresses_sf = []
+File.readlines("/Users/laurynporte/Desktop/finalProject/Scene/db/latlnges.txt").each do |address|
+	random_addresses_sf << address.strip
+end
+
+# SF Artists from City Arts
+response = HTTParty.get('https://www.kimonolabs.com/api/aeongt2c?apikey=OJQIQILpmhaUZHWM2iTIHALjlCkYOIHf')
+
+response["results"]["collection1"].each do |artist|
+	avatar = HTTParty.get('http://uifaces.com/api/v1/random')
+	avatar_img = avatar["image_urls"]["bigger"]
+	new_artist = Artist.create(
+		name: artist["category"]["text"],
+		email: artist["category"]["href"],
+		password: "123456",
+		avatar: avatar_img,
+		bio: ["My work explores the relationship between the Military-Industrial Complex and vegetarian ethics.", "With influences as diverse as Rousseau and Roy Lichtenstein, new tensions are manufactured from both constructed and discovered textures.", "Ever since I was a pre-adolescent I have been fascinated by the unrelenting divergence of the moment. What starts out as triumph soon becomes corroded into a dialectic of power, leaving only a sense of dread and the dawn of a new beginning.", "As shimmering forms become transformed through studious and repetitive practice, the viewer is left with an insight into the possibilities of our culture.", "As spatial forms become distorted through studious and academic practice, the viewer is left with a clue to the darkness of our culture."].sample,
+		address: random_addresses_sf.sample,
+		facebook: "www.facebook.com/" + artist["category"]["text"],
+		twitter: "@" + artist["category"]["text"] + "ger",
+		website: artist["website"]["href"]
+		)
+
+	Piece.create(
+		img: artist["images"]["src"],
+		title: artist["images"]["alt"],
+		description: random_descriptions.sample(3).to_s,
+		artist_id: new_artist.id,
+		tag_list: random_tags.sample(3).to_s	
+		)
+	end
 
