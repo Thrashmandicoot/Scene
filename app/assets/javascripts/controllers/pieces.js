d22c247c38639f8a05b1fb58c2e7cb099d8e636c
Andrew Pham <email": null>
2015-08-30 20:54:16 -0700

    start angular module and controller for show pieces

--- app/assets/javascripts/controllers/pieces.js
+++ app/assets/javascripts/controllers/pieces.js
@@ -1,7 +1,18 @@
-var pieceShow = angular.module('showPiece', []);
+var showPiece = angular.module('ShowPiece', []);
 
-pieceShow.controller('PieceCtrl', function(){
+
+showPiece.controller('PieceCtrl', function($scope, $http){
+  $scope.testing = "Scoping with Angular"
   this.elmt = test
+
+
+  function getArtist() {
+    return $http.get("/artists/10/pieces/4").success(function(data){
+      console.log(data)
+    })
+  }
+
+  getArtist();
 })
 
 var test = {
