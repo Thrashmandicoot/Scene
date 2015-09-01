0b6eb6a33f442acaba799f9e6b1f86e809215cfa
Ashton Alexander <ashton.alexander@gmail.com>
2015-08-31 21:41:10 -0700

    Add parallax background refactor

---
+++ app/assets/javascripts/parallax.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var jumboHeight = $('.divotron').outerHeight();
+  function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+  }
+
+  $(window).scroll(function(e){
+      parallax();
+  });
+});
