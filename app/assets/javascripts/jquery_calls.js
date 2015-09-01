edff840a74100409799ea81e33b55c482b2fcad1
Andrew Pham <email": null>
2015-09-01 09:44:16 -0700

    center selected image

--- app/assets/javascripts/jquery_calls.js
+++ app/assets/javascripts/jquery_calls.js
@@ -7,6 +7,8 @@
   var showPiece = function(){
     $(".pic").on('click', function(event){
       event.preventDefault();
+      smoothScroll();
+
       var url = $(this).attr("href")
       var request = $.ajax({
         url: url,
@@ -16,8 +18,6 @@
       request.done(function(response){
         console.log("hit")
         $('.r-this').remove()
-        // $('.allpics').hide()
-        // console.log(url)
         $('#target').append(response)
         closeButton();
       })
@@ -35,10 +35,19 @@
 // zoom hover
 var zoomHover = function(){
   $('.viewport').mouseenter(function(e) {
-          $(this).children('a').children('img').animate({ height: '322', left: '-10', top: '-10', width: '490'}, 100);
+          $(this).children('a').children('img').animate({ height: '16em', left: '-5', top: '-5', width: '21em'}, 100);
           $(this).children('a').children('span').fadeIn(200);
       }).mouseleave(function(e) {
-          $(this).children('a').children('img').animate({ height: '299', left: '0', top: '0', width: '450'}, 200);
+          $(this).children('a').children('img').animate({ height: '15em', left: '0', top: '0', width: '20em'}, 200);
           $(this).children('a').children('span').fadeOut(200);
       });
 }
+
+// Smooth Scroll
+var smoothScroll = function(){
+    window.scrollTo(0,165)
+}
+
+
+
+
