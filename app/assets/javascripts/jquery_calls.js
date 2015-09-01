5c4bdaf6454677ec4fa124e49e834836385be814
Andrew Pham <email": null>
2015-09-01 14:33:01 -0700

    center the show image, kinda

--- app/assets/javascripts/jquery_calls.js
+++ app/assets/javascripts/jquery_calls.js
@@ -32,8 +32,9 @@
 // zoom hover
 var zoomHover = function(){
   $('.viewport').mouseenter(function(e) {
-          $(this).children('a').children('img').animate({ height: '16em', left: '-5', top: '-5', width: '21em'}, 100);
+          $(this).children('a').children('img').animate({ height: '16em', left: '-10', top: '-10', width: '21em'}, 100);
           $(this).children('a').children('span').fadeIn(200);
+
       }).mouseleave(function(e) {
           $(this).children('a').children('img').animate({ height: '15em', left: '0', top: '0', width: '20em'}, 200);
           $(this).children('a').children('span').fadeOut(200);
